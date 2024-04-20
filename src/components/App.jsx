de34a7b9ee05808849ffabad3b457098c4eb7699
Nicolai <134018297+NicolaiTesla4@users.noreply.github.com>
2024-04-20 15:55:00 -0500

    Six

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+import { filterUser } from '../redux/contacts-selectors.js'
+import { useSelector, useDispatch } from 'react-redux'
+import { useState } from 'react'
+import ContactForm from './contact-form/ContactForm.jsx'
+import ContactList from './contact-list/ContactList.jsx'
+import Filter from './filter/Filter.jsx'
+import css from './App.module.css';
+
+
+export default function App() {
+  
+  const users = useSelector((state) => state.users.contacts)
+  const [filteredContact, setFilteredContacts]= useState('')
+  const dispatch = useDispatch()
+
+  function handleFilter(e){ 
+    
+    dispatch(filterUser(e.target.value))
+    setFilteredContacts(users.filter((user) =>
+            user.name.toLowerCase().includes(e.target.value.toLowerCase())
+        )
+      )
+    }
+
+return (
+    <div className={css.container}>
+        <h1>Phonebook</h1>
+        <ContactForm />      
+        <h2>Contacts</h2>
+      <Filter handleFilter={handleFilter}/>
+      <ContactList filteredContact={filteredContact } />
     </div>
-  );
-};
+  )
+}
